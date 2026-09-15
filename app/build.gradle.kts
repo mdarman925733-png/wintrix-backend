dcb7070da1a679a5ce54b5b5a4b4381bbf2712ce
mdarman925733-png <mdarman925733@gmail.com>
2026-09-15 16:00:26 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.aicomp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.1.4"
+        versionCode = 6
+        versionName = "1.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
