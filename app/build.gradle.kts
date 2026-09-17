b8b12524a4416f7268e84b7b1590bfcad88b647a
mdarman925733-png <mdarman925733@gmail.com>
2026-09-17 06:23:32 +0530

    Update versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.aicomp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 18
-        versionName = "1.7.17"
+        versionCode = 19
+        versionName = "1.7.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
